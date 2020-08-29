b463dca3dee3a1775a780a564f22c3a242db52db
arpit <arpit.savarkar10@gmail.com>
2020-08-29 13:18:48 -0600

    Updated tests

--- bit_operations.c
+++ bit_operations.c
@@ -19,198 +19,392 @@
  * @author Arpit Savarkar
  * @date August 27 2020
  * @version 1.0
+ * 
+ * 
 
-https://www.geeksforgeeks.org/program-to-reverse-a-string-using-pointers/
+  Sources of Reference :
+  Links :https://stackoverflow.com/questions/7775991/how-to-get-hexdump-of-a-structure-data
+  Textbooks : Embedded Systems Fundamentals with Arm Cortex-M based MicroControllers 
 
+  I would like to thank the SA's for the course Rakesh Kumar, Saket Penurkar for their 
+  support. 
 
 */
 
 #include "bit_operations.h"
 
-#define MASK(x) (1UL << (x))
-#define hex_asc_lo(x)	hex_asc[((x) & 0x0f)]
-#define hex_asc_hi(x)	hex_asc[((x) & 0xf0) >> 4]
 
-// Helper Functions to convert to Hexadecimal
-char rem(int num) 
-{ 
+// ************************ Helper Functions  ************************************
+
+char convert(int num)  { 
+    /*
+    Conversion Table for reference
+    Decimal:	0	1	2	3	4	5	6	7
+    Hex	        0	1	2	3	4	5	6	7
+    Decimal:	8	9	10	11	12	13	14	15
+    Hex      	8	9	A	B	C	D	E	F
+    */ 
     if (num >= 0 && num <= 9) 
         return (char)(num + '0'); 
     else
         return (char)(num - 10 + 'A'); 
 } 
 
-// Helper Function to set nth bit of input 
-int set_bit(uint32_t input, int bit) 
-{ 
-    return (input | (1 << (bit))); 
+int set_bit(uint32_t input, int bit)  { 
+    // Returns Input manipulated to set a bit
+    //  "|" is Bitwise OR
+    return (input | (1U << (bit))); 
 } 
   
-// Helper Function to clear nth bit of input 
-int clear_bit(uint32_t input, int bit) 
-{ 
-    return (input & (~(1 << (bit)))); 
+
+int clear_bit(uint32_t input, int bit)  {
+    // Returns Input manipulated to clear a bit 
+    //  "&" is Bitwise AND
+    return (input & (~(1U << (bit)))); 
 } 
   
-// Helper Function to toggle the nth bit of input 
-int toggle_bit(uint32_t input, int bit) 
-{ 
-    return (input ^ (1 << (bit))); 
+
+int toggle_bit(uint32_t input, int bit)  { 
+    // Returns Input manipulated to toggle a bit 
+    // "^" is Bitwise AND
+    return (input ^ (1U << (bit))); 
 }
 
-int uint_to_binstr(char *str, size_t size, uint32_t num, uint8_t nbits) {
+int check_legality(char *str, size_t size, uint32_t num, uint8_t nbits, 
+                   int base) {
 
-    int base = 2, len = 0, i =0;
-    str[0] = '0';
+    // Function to prevent segmentation fault to acess 
+
+    int len = 0;
+
+    if( (nbits/8) > size )
+        return -1;
+
+    // Illegal nbits
+    if (nbits == 0 ) {
+        str[0] = '\0';
+        return -1;
+    }
+
+    int temp = num;
+    while (temp>0) { // Returns the modulo as binary for base 2
+        temp /= base;
+        len++;
+    }
+
+    if (len > nbits) {
+        str[0] = '\0';
+        return -1;
+    }
+}
+
+void dec_to_bin(char *str, size_t size, uint32_t num, uint8_t nbits) {
+
+    int base = 2, i =0, len = 0;
+
+    // To specify the 0bxxxxxx for the binary 
+    str[0] = '0';  
     str[1] = 'b';
     
+    // Instantiating string with '0's upto nbytes
     for (i =2; i < nbits+2; i++) {
         str[i] = '0';
     }
+    //Demarkating End of string
     str[i] = '\0';
 
-    for (i =2; i < nbits+2; i++) {
-        str[i] = '0';
-    }
-    str[i] = '\0';
+    // Need to be stored backwards for correctness
     while (num>0) {
-        str[--i] = rem(num % base);
-        num /= base;
-        len ++;
-    }
-    if (nbits ==0 ) {
-        printf("Illegal bits");
-        *str = NULL;
-        return -1;
-    } 
-    if (len > nbits) {
-        printf("num too large");
-        *str = NULL;
-        return -1;
-    }
-    len=0;
-    for (i =0 ; str[i]!= '\0'; i++)
-        len++;  
-
-    return len;
-}
-
-int int_to_binstr(char *str, size_t size, int32_t num, uint8_t nbits) {
-
-    char *start = str;
-    if (num>0) 
-        return uint_to_binstr(str, size, num, nbits);
-
-    int base = 2;
-    int i =0,  c = 1;
-    char *p = str;
-    *(p++) = '0';
-    *(p++) = 'b';
-    char *shifted_p;
-    shifted_p = p;
-
-    for (int i =0; i < nbits; i++) 
-        *(p++) = '0';
-    *(p) = '\0';
-    char *t = (--p);
-    int temp, len=0;
-    num*=-1;
-    while(num>0) {
-        *t = rem(num % base);
-        t--;
+        str[--i] = convert(num % base); // Returns the modulo as binary for base 2
         num /= base;
         len++;
     }
 
-    if (nbits ==0 ) {
-        printf("Illegal bits");
-        *str = NULL;
-        return -1;
-    } 
+    // To prevent Segmentation faults restricted to nbits 
     if (len > nbits) {
-        printf("num too large");
-        *str = NULL;
+        str[0] = '\0';
+    }
+
+}
+
+
+int uint_to_binstr(char *str, size_t size, uint32_t num, uint8_t nbits) {
+
+
+    int len = 0;
+
+    if(num<0)
+        return -1;
+    if (check_legality(str, size, num, nbits, 2) == -1)
+        return -1;
+    dec_to_bin(str, size, num, nbits);
+
+    // To prevent Segmentation faults restricted to nbits 
+    if (len > nbits) {
+        str[0] = '\0';
         return -1;
     }
+    len = 0;
+    for (int i =0; str[i]!='\0'; i++)
+        len++;
 
-    // 1's complement logic
-    p = shifted_p;
-    while(*p != '\0') {
-        if(*p == '1')
-            *(p++) = '0';
+    return (len);
+}
 
-        else if(*p == '0')
-            *(p++) = '1';
-    }
-    *(p--) = '\0';
+
+void test_uint_to_binstr() {
+    size_t size = 1024;
+    char str[size];
+    int k =0;
+    int ret,i;
+        
+    ret = uint_to_binstr(str, size, INT8_MIN, 8);
+        if(ret == -1) 
+            return 0;
+
+    ret = uint_to_binstr(str, size, INT8_MAX, 8);
+        if(ret != -1) 
+            return 0;
+
+    ret = uint_to_binstr(str, size, 0, 16);
+        if(ret == -1) 
+            return 0;
+
+    ret = uint_to_binstr(str, size, UINT16_MAX+1, 8);
+        if(ret != -1) 
+            return 0;
+
+
+    // Compiler interprets UINT32_MAX as -1 when assigned to uint32_t
+    // which results in legality check to be as negative number 
+
+    ret = uint_to_binstr(str, size, UINT32_MAX, 8);
+        if(ret != 0) 
+            return 0;
+
+    ret = uint_to_binstr(str, size, UINT32_MAX, 16);
+        if(ret != 0) 
+            return 0;
+
+    ret = uint_to_binstr(str, size, UINT32_MAX, 32);
+        if(ret == 0) 
+            return 0;
+
+    return 1;
+
+}
     
-    while( (nbits--) != 1 ) {
-        if(*p == '1' && c == 1)
-        {
-            *(p--) = '0';
+
+
+int int_to_binstr(char *str, size_t size, int32_t num, uint8_t nbits) {
+
+    // If Unsigned Integer uint_to_binstr() can be used
+    if (num>0) 
+        return uint_to_binstr(str, size, num, nbits);
+
+    if (check_legality(str, size, num*-1, nbits, 2) == -1)
+        return -1;
+
+    num *= -1;
+    int i =0,  c = 1;
+
+    dec_to_bin(str, size, num, nbits);
+
+    for(i =2; str[i]!='\0'; i++) {
+        if(str[i] == '1')
+            str[i] = '0';
+
+        else if(str[i] == '0')
+            str[i] = '1';
+    }
+    int k = i-1;
+    for (i = k; i>=0; i--) {
+        if(str[i] == '1' && c == 1) {
+            str[i] = '0';
         }
-        else if(*p == '0' && c == 1)
-        {
-            *(p--) = '1';
+        else if(str[i] == '0' && c == 1) {
+            str[i] = '1';
             c = 0;
         }
     }
 
-    p = str;
-    len=0;
-    while (*(p++) != '\0')
+    int len = 0;
+    for (int i =0; str[i]!='\0'; i++)
         len++;
 
-    return len;
+    return (len);
 }
+
+void test_int_to_binstr() {
+    size_t size = 1024;
+    char str[size];
+    uint8_t nbits = 16;
+    int k =0;
+    int ret,i;
+        
+    ret = int_to_binstr(str, size, INT8_MIN*2, 8);
+        if(ret != -1) 
+            return 0;
+    
+    ret = int_to_binstr(str, size, INT8_MIN, 8);
+        if(ret == -1) 
+            return 0;
+
+    ret = int_to_binstr(str, size, INT8_MAX*2+2, 8);
+        if(ret != -1) 
+            return 0;
+
+    ret = int_to_binstr(str, size, INT8_MAX, 8);
+        if(ret == -1) 
+            return 0;
+
+
+
+    ret = int_to_binstr(str, size, INT16_MIN*2, 16);
+        if(ret != -1) 
+            return 0;
+    
+    ret = int_to_binstr(str, size, INT16_MIN, 16);
+        if(ret == -1) 
+            return 0;
+
+    ret = int_to_binstr(str, size, INT16_MAX*2+2, 16);
+        if(ret != -1) 
+            return 0;
+
+    ret = int_to_binstr(str, size, INT16_MAX, 16);
+        if(ret == -1) 
+            return 0;
+
+
+    // Compiler interprets UINT32_MAX as -1 when assigned to uint32_t
+    // which results in legality check to be as negative number 
+
+    ret = int_to_binstr(str, size, INT32_MIN+1, 8);
+        if(ret != -1) 
+            return 0;
+
+    ret = int_to_binstr(str, size, INT32_MIN+1, 16);
+        if(ret != -1) 
+            return 0;
+
+    ret = int_to_binstr(str, size, INT32_MIN+1, 32);
+        if(ret == 0) 
+            return 0;
+
+    ret = int_to_binstr(str, size, INT32_MAX-1, 8);
+        if(ret != -1) 
+            return 0;
+
+    ret = int_to_binstr(str, size, INT32_MAX-1, 16);
+        if(ret != -1) 
+            return 0;
+
+    ret = int_to_binstr(str, size, INT32_MAX-1, 32);
+        if(ret == 0) 
+            return 0;
+
+    return 1;
+
+}
+
 
 int uint_to_hexstr(char *str, size_t size, uint32_t num, uint8_t nbits) {
 
     int base = 16, len = 0, i =0;
-    int k = 0;
-    // char *p = str;
-    str[k++] = '0';
-    str[k++] = 'x';
+    int k = 2;
 
-    for (i=0; i < (nbits+2)/4; i++) 
+    if (check_legality(str, size, num, nbits, base) == -1)
+        return -1;
+
+    if (nbits/4 == 0 ) {
+        str[0] = '\0';
+        return -1;
+    } 
+
+    int temp = num;
+    while (temp>0) { // Returns the modulo as binary for base 2
+        temp /= base;
+        len++;
+    }
+
+    if (len > nbits/4) {
+        str[0] = '\0';
+        return -1;
+    }
+
+    str[0] = '0';
+    str[1] = 'x';
+
+    for (i=0; i < nbits/4; i++) {
         str[k++] = '0';
+        printf("i :%d\n", i);
+    }
     str[k] = '\0';
-    char *t ;
-    t = &str[--k];
     while(num>0) {
-        *t = rem(num % base);
-        t--;
+        char rem = convert(num % base);
+        // printf("%c", rem);
+        str[--k] = convert(num % base);
         num /= base;
         len++;
     }
-    if (nbits /4 != 0 ) {
-        printf("Illegal bit size");
-        *str = NULL;
-        return -1;
-    } 
-    if (len > nbits) {
-        printf("num too large");
-        *str = NULL;
-        return -1;
-    }
+    // str[++k] = '\0';
 
     len = 0;
     for(i =0; str[i]!='\0'; i++) {
+        printf("%c", str[i]);
         len++;
     }
 
     return len;
-} 
+}
 
-typedef enum {
-CLEAR,
-SET,
-TOGGLE
-} operation_t;
+void test_uint_to_hexstr() {
+    size_t size = 1024;
+    char str[size];
+    int ret;
+        
+    ret = uint_to_hexstr(str, size, INT8_MIN, 8);
+        if(ret == -1) 
+            return 0;
+
+    ret = uint_to_hexstr(str, size, INT8_MAX, 8);
+        if(ret != -1) 
+            return 0;
+
+    ret = uint_to_hexstr(str, size, 0, 16);
+        if(ret == -1) 
+            return 0;
+
+    ret = uint_to_hexstr(str, size, UINT16_MAX, 8);
+        if(ret != -1) 
+            return 0;
+
+
+    // Compiler interprets UINT32_MAX as -1 when assigned to uint32_t
+    // which results in legality check to be as negative number 
+
+    ret = uint_to_hexstr(str, size, UINT32_MAX, 8);
+        if(ret != -1) 
+            return 0;
+
+    ret = uint_to_hexstr(str, size, UINT32_MAX, 16);
+        if(ret != -1) 
+            return 0;
+
+    ret = uint_to_hexstr(str, size, UINT32_MAX, 32);
+        if(ret == -1) 
+            return 0;
+
+    return 1;
+
+}
+
+
 uint32_t twiggle_bit(uint32_t input, int bit, operation_t operation) {
 
     if ( bit <0 || bit > 31)
-        return -1;
+        return 0xFFFFFFFF;
 
     if (operation == CLEAR) {
         return clear_bit(input, bit);
@@ -227,43 +421,92 @@ uint32_t twiggle_bit(uint32_t input, int bit, operation_t operation) {
 
 }
 
+void test_twiggle_bit() {
+
+    uint32_t input = UINT32_MAX-2;
+    uint32_t output;
+    if (twiggle_bit(input, 0, CLEAR) ==  0xFFFFFFFF )
+        return 0;
+
+    if (twiggle_bit(input, 0, SET) ==  0xFFFFFFFF )
+        return 0;
+
+    if (twiggle_bit(input, 0, TOGGLE) ==  0xFFFFFFFF )
+        return 0;
+
+    if (twiggle_bit(input, 32, CLEAR) ==  0xFFFFFFFF )
+        return 0;
+
+    if (twiggle_bit(input, 32, SET) ==  0xFFFFFFFF )
+        return 0;
+
+    if (twiggle_bit(input, 32, TOGGLE) ==  0xFFFFFFFF )
+        return 0;
+
+    if (twiggle_bit(input, -1, CLEAR) ==  0xFFFFFFFF )
+        return 0;
+
+    if (twiggle_bit(input, -1, SET) ==  0xFFFFFFFF )
+        return 0;
+
+    if (twiggle_bit(input, -1, TOGGLE) ==  0xFFFFFFFF )
+        return 0;
+
+    return 1;
+
+
+}
+
+
 uint32_t grab_three_bits(uint32_t input, int start_bit) {
 
     uint32_t output;
     int num_elem = 3;
-    int output = (((1 << num_elem) - 1) & (input >> (start_bit))); 
-    if (output < 0)
+    if (start_bit < 0 || start_bit >= 30)
         return 0xFFFFFFFF;
-    else 
+    output = (((1 << num_elem) - 1) & (input >> (start_bit))); 
+    
         return output;
-
 }
 
-char *hexdump(char *str, size_t size, const void *loc, size_t nbytes) {
+void test_grab_three_bits() {
 
+    uint32_t input = UINT32_MAX-2;
+    uint32_t output;
+    if (grab_three_bits(input, 0) ==  0xFFFFFFFF )
+        return 0;
+
+    if (grab_three_bits(input, 30) !=  0xFFFFFFFF )
+        return 0;
+
+    if (grab_three_bits(input, -1)  !=  0xFFFFFFFF )
+        return 0;
+
+    return 1;
+}
+
+
+char *hexdump(char *str, size_t size, const void *loc, size_t nbytes) {
 
     if (nbytes > size) {
         str = '\0';
         return str;
     }
-        
-    int i;
-    unsigned char buff[17];
-    const unsigned char * pc = (const unsigned char *)loc;
-    int k = 0;
 
     // Length checks.
 
-    if (nbytes == 0) {
+    if (nbytes <= 0) {
         printf("  ZERO LENGTH\n");
-        return;
-    }
-    else if (nbytes < 0) {
-        printf("  NEGATIVE LENGTH: %d\n", nbytes);
-        return;
+        str[0]='\0';
+        return -1;
     }
 
-    // Process every byte in the data.
+    int i, j;
+    char temp[3];  // Required to restrict compiler from using 2 bytes for special characters
+    uint8_t rem = 0, num;
+    unsigned char buff[17]; // String of 17 
+    const unsigned char * pc = (const unsigned char *)loc;
+    int k = 0;
 
     for (i = 0; i < nbytes; i++) {
         // Multiple of 16 means new line (with line offset).
@@ -281,32 +524,62 @@ char *hexdump(char *str, size_t size, const void *loc, size_t nbytes) {
             if(i == 0)
                 str[k++] = '0';
             
-            str[k++] = i;
+            num = i;
+            do 
+            { 
+                int rem = num % 16; 
+                str[k++] = (rem > 9)? rem -10 + 'A' : rem + '0'; 
+                num = num/16; 
+            } while (num != 0);
+            
             for (int s =0; s<2; s++)
                 str[k++] = ' ';
         }
 
         // Now the hex code for the specific character.
-        str[k++] = pc[i];
+        str[k++] = '0';
+        str[k++] = '0';
+        // k+=;
+        num = pc[i];
+        for(j=0; temp[j]!='\0'; j++)
+            temp[j] = '0';
+        j = 0;
+        while (num != 0)
+        {   
+            rem = num % 16;
+            temp[j++] =  (rem > 9)? (rem-10) + 'A' : rem + '0' ; 
+            num = num/16; 
+        }
+        j = 0;
+        // temp[j] = '\0';
+        for(j = 0; j<=1; j++)
+            str[--k] = temp[j];
+        k+=2;
         str[k++] = ' ';
-
         // And buffer a printable ASCII character for later.
 
-        if ((pc[i] < 0x20) || (pc[i] > 0x7e)) 
+        if ((pc[i] > 0x20) || (pc[i] < 0x7e)) 
             buff[i % 16] = '.';
         else
             buff[i % 16] = pc[i];
-        buff[(i % 16) + 1] = '\0';
+        buff[(i % 16) + 1] = '\n';
     }
 
     // Pad out last line if not exactly 16 characters.
 
     while ((i % 16) != 0) {
-        str[k++] = 0x20;
+        str[k++] = ' ';
         i++;
     }
+    return str;
+}
+
+
+void test_hexdump() {
 
 }
+
+
 
 int main(int argc, char** argv) {
 
--- bit_operations.h
+++ bit_operations.h
@@ -1,16 +1,12 @@
 #ifndef BIT_OPERATIONS_
 #define BIT_OPERATIONS_
 
+#include <stdint.h>
+#include <stddef.h>
 
-#define CHAR_MIN 0
-#define CHAR_MAX 255
-#define UINT_MIN -32768
-
-#include <stdlib.h>
-#include <inttypes.h>
-#include <stdbool.h>
-#include <ctype.h>
-// UINT32_MAX
+// #define INT_MIN -2147483648
+// #define INT_MAX +2147483647
+// #define UINT_MAX 4294967295
 
 
 typedef enum {
@@ -19,13 +15,157 @@ SET,
 TOGGLE
 } operation_t;
 
-// Helper Functions 
-void stringlen(char* str);
-//  
+/**
+​ * ​ ​ @brief​ ​ Returns a character corresponding to hex table for decimal equivalent
+​ *
+​ * ​ ​ Given​ ​ a ​ ​ integer ​ to​ ​ a ​ ​ char​ ​ data​ ​ set,​ ​ this​ ​ will​ ​ return​ ​ a char 
+​ * ​ equivalent in hex
+​ *
+​ * ​ ​ @param​ ​ num ​ Integer ​ to​ ​ a ​ ​ data​ ​ item
+​ *
+​ * ​ ​ @return​ ​ char.
+​ */
+char convert(int num); 
+
+/**
+​ * ​ ​ @brief​ ​ Returns a integer set with a specific bit
+​ *
+​ * ​ ​ Given​ ​ a ​ ​ integer ​and a bit location,​ ​ this​ ​ will​ ​ return​ ​ a uint32_t
+ *   Set to a specified bit location
+ *
+​ * ​ ​ @param​ ​ num : ​ Integer ​ to​ ​ a ​ ​ data​ ​ item
+ *   @param  bit : Location of a specific bit on the register corresponding to num
+​ *
+​ * ​ ​ @return​ ​ uint32_t
+​ */
+int set_bit(uint32_t input, int bit);
+
+/**
+​ * ​ ​ @brief​ ​ Returns a integer set with a specific bit
+​ *
+​ * ​ ​ Given​ ​a integer ​and a bit location,​ this​ ​ will​ ​ return​ ​ a uint32_t
+ *   cleared to a specified bit location
+ *
+​ * ​ ​ @param​ ​ num : ​ Integer ​ to​ ​ a ​ ​ data​ ​ item
+ *   @param  bit : Location of a specific bit on the register corresponding to num
+​ *
+​ * ​ ​ @return​ ​ uint32_t
+​ */
+int clear_bit(uint32_t input, int bit) ;
+
+/**
+​ * ​ ​ @brief​ ​ Returns a integer set with a specific bit
+​ *
+​ * ​ ​ Given​ ​a integer ​and a bit location,​ this​ ​ will​ ​ return​ ​ a uint32_t
+ *   Toggled to a specified bit location
+ *
+​ * ​ ​ @param​ ​ num : ​ Integer ​ to​ ​ a ​ ​ data​ ​ item
+ *   @param  bit : Location of a specific bit on the register corresponding to num
+​ *
+​ * ​ ​ @return​ ​ uint32_t
+​ */
+int toggle_bit(uint32_t input, int bit);
+
+/**
+​ * ​ ​ @brief​ ​ Returns a pointer to a string corresponding to binary representation of 
+ *           unsigned uint32_t integer
+​ *
+​ * ​ ​ Given​ ​a pointer to string instantiated with a specified size, function returns the 
+ *   length of the binary equivalent of a number (argument) upto a specified number of 
+ *   bits (argument)
+ *
+​ * ​ ​ @param​ ​ str : ​ Pointer to a char​ ​ data​ ​ set 
+ *   @param  size : char array Instantiated of at 'size' bytes
+ *   @param  num : Integer to be converted to binary 
+ *   @param nbits : It is the number of bits of the input
+​ *
+​ * ​ ​ @return​ ​ int
+​ */
+
 int uint_to_binstr(char *str, size_t size, uint32_t num, uint8_t nbits);
+
+/**
+​ * ​ ​ @brief​ ​ Returns a pointer to a string corresponding to binary representation of 
+ *           signed uint32_t integer
+​ *
+​ * ​ ​ Given​ ​a pointer to string instantiated with a specified size, function returns the 
+ *   length of the binary equivalent of a number (argument) upto a specified number of 
+ *   bits (argument)
+ *
+​ * ​ ​ @param​ ​ str : ​ Pointer to a char​ ​ data​ ​ set 
+ *   @param  size : char array Instantiated of at 'size' bytes
+ *   @param  num : Integer to be converted to binary 
+ *   @param nbits : It is the number of bits of the input
+​ *
+​ * ​ ​ @return​ ​ int
+​ */
+
 int int_to_binstr(char *str, size_t size, int32_t num, uint8_t nbits);
+
+
+/**
+​ * ​ ​ @brief​ ​ Returns a pointer to a string corresponding to hexadecimal representation of 
+ *           unsigned uint32_t integer
+​ *
+​ * ​ ​ Given​ ​a pointer to string instantiated with a specified size, function returns the 
+ *   length of the hex equivalent of a number (argument) upto a specified number of 
+ *   bits (argument)
+ *
+​ * ​ ​ @param​ ​ str : ​ Pointer to a char​ ​ data​ ​ set 
+ *   @param  size : char array Instantiated of at 'size' bytes
+ *   @param  num : Integer to be converted to binary 
+ *   @param nbits : It is the number of bits of the input
+​ *
+​ * ​ ​ @return​ ​ int
+​ */
+
 int uint_to_hexstr(char *str, size_t size, uint32_t num, uint8_t nbits);
+
+/**
+​ * ​ ​ @brief​ ​ Bit Manipulation to set/clear/toggle a but at a specified bit location
+​ *
+​ * ​ ​ Changes a single bit of the input value, without changing the other bits. Upon invocation, bit is in
+*    the range 0 to 31, inclusive. Returns 0xFFFFFFFF in the case of an error.
+ *
+​ * ​ ​ @param​ ​ input : Integer data of whose bits are to be manipulated 
+ *   @param  bit : Specific location upon which bit manipulation is to be carried out
+​ *   @param  operation : Object to Enum Operation_t
+​ * ​ ​ @return​ ​ uint32_t
+​ */
+typedef enum {
+CLEAR,
+SET,
+TOGGLE
+} operation_t;
 uint32_t twiggle_bit(uint32_t input, int bit, operation_t operation);
+
+/**
+​ * ​ ​ @brief​ ​ Bit Manipulation to return three bits from the input value, shifted down. 
+ *
+​ * ​ ​ @param​ ​ input : ​ Pointer to a integer data​ ​over which bits values are to be extracted 
+ *   @param  start_bit : Starting bit location from which next 3 bits would be extracted
+​ *
+​ * ​ ​ @return​ ​ uint32_t 
+​ */
+
 uint32_t grab_three_bits(uint32_t input, int start_bit);
+/**
+​ * ​ ​ @brief​ ​ Hex Dump of a memory location upto a selected number of bytes at a specified memory 
+ *           location
+​ *
+​ * ​ ​ Returns a string pointer representing a “dump” of the nbytes of memory starting at loc. Bytes are
+ *    printed up to 16 bytes per line, separated by newlines. The function returns the pointer str, which 
+ *   facilitates daisy-chaining this function into other
+ *   string-manipulation functions such as puts.
+ *
+​ * ​ ​ @param​ ​ str : ​ Pointer to a char​ data​ set where the hex dump would be stored
+ *   @param  size : char array Instantiated of at 'size' bytes
+ *   @param  num : Address in memory from where hex dump would be recorded
+ *   @param nbytes : Number of bytes upto which the hex values of the memory would be stored
+ * 
+​ * ​ ​ @return​ ​ Character Pointer
+​ */
+
 char *hexdump(char *str, size_t size, const void *loc, size_t nbytes);
-#endif
+
+#endif /* BIT_OPERATIONS_ */
